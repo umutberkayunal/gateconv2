af4920e1f37b277226f9051847b8914cc477ba9d
umutberkayunal <umutberkayunal@gmail.com>
2023-05-20 23:34:12 +0300

    added background

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,45 +1,106 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
+import React from 'react';
+import { View, Image, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
 const Header = () => {
   return (
-    <View style={styles.container}>
-     <TouchableOpacity>
-      <Image 
-       style={styles.logo}
-       source={require('C:/gateconv2/assets/header-logo.png')}
-       />
-     </TouchableOpacity> 
-     
-     <View style={styles.iconsContainer}>
-    
+    <ImageBackground
+      source={require('C:/gateconv2/assets/background.png')}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Image
+            source={require('C:/gateconv2/assets/profileicon.png')}
+            style={styles.profileicon}
+          />
+        </TouchableOpacity>
 
-     </View>
+        <TouchableOpacity>
+          <Image
+            style={styles.logo}
+            source={require('C:/gateconv2/assets/header-logo.png')}
+          />
+        </TouchableOpacity>
 
-    </View>
-  )
-}
+        <View style={styles.iconsContainer}>
+          <TouchableOpacity>
+            <Image
+              source={require('C:/gateconv2/assets/likeicon.png')}
+              style={styles.likeicon}
+            />
+          </TouchableOpacity>
 
+          <TouchableOpacity>
+            <View style={styles.unreadBadge}>
+              <Text style={styles.unreadBadgeText}>11</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Image
+              source={require('C:/gateconv2/assets/messageicon.png')}
+              style={styles.messageicon}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        flexDirection: 'row',
-        marginHorizontal: 20,
-    },
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+  },
+  logo: {
+    marginTop: -40,
+    marginLeft: 10,
+    width: 300,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  profileicon: {
+    marginLeft: -15,
+    marginTop: -28,
+    width: 25,
+    height: 25,
+  },
+  messageicon: {
+    marginTop: -32,
+    marginRight: 20,
+    width: 30,
+    height: 30,
+  },
+  likeicon: {
+    marginTop: -32,
+    marginRight: 17,
+    width: 30,
+    height: 30,
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  unreadBadge: {
+    backgroundColor: '#FF3250',
+    position: 'absolute',
+    left: 20,
+    bottom: 18,
+    width: 25,
+    height: 18,
+    borderRadius: 25,
+    alignItems: 'center',
+  },
+  unreadBadgeText: {
+    color: '#FFFFFF',
+    fontSize: 12,
+  },
+});
 
-    iconsContainer: {
-      flexDirection: 'row',
-    },
-
-    logo: {
-        marginTop: -50,
-        marginLeft: -120,
-        width: 300,
-        height: 150,
-        resizeMode: 'contain',
-    },
-})
-
-export default Header
+export default Header;
