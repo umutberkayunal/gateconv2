51bf8d6587a43cb5ed9cfc9ed729223f8788e346
umutberkayunal <umutberkayunal@gmail.com>
2023-05-20 18:36:53 +0300

    added logo and icons

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,12 +1,45 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
 
 const Header = () => {
   return (
-    <View>
-      <Text style={{ color:'white' }}>HEADER</Text>
+    <View style={styles.container}>
+     <TouchableOpacity>
+      <Image 
+       style={styles.logo}
+       source={require('C:/gateconv2/assets/header-logo.png')}
+       />
+     </TouchableOpacity> 
+     
+     <View style={styles.iconsContainer}>
+    
+
+     </View>
+
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20,
+    },
+
+    iconsContainer: {
+      flexDirection: 'row',
+    },
+
+    logo: {
+        marginTop: -50,
+        marginLeft: -120,
+        width: 300,
+        height: 150,
+        resizeMode: 'contain',
+    },
+})
 
 export default Header
